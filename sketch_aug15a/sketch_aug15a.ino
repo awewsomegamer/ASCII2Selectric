20d3b48e2952d1893194b066a143bec82bcf7d82
awewsomegamer <awewsomegamer@gmail.com>
2023-08-16 22:19:40 -0700

    Added communication pause capability, more pins for more functionality, and breathing room for the Selectric. I need to look further int when exactly the pins should be pulled and released.

--- sketch_aug15a/sketch_aug15a.ino
+++ sketch_aug15a/sketch_aug15a.ino
@@ -3,27 +3,35 @@
 
 // These names are extracted from "IBM Selectric APM - Nov 1980.pdf"
 // Page 58, PDF Page 60
-#define TILT1_PIN          5
-#define TILT2_PIN          6
-#define ROTATE1_PIN        7
-#define ROTATE2_PIN        8
-#define ROTATE2A_PIN       9
-#define NROTATE_PIN        10
-#define SHIFT_PIN          11
-#define SPACE_PIN          12
-#define CHAR_SEND_PIN      13
-#define DEFUALT_CHAR_COUNT 44
-#define TIME_TO_PRINT_CHAR 1000 // Place holder, this is the time in
-                                // milliseconds that it takes for the
-                                // Selectric takes to print one character
+#define TILT1_PIN          2
+#define TILT2_PIN          3
+#define ROTATE1_PIN        4
+#define ROTATE2_PIN        5
+#define ROTATE2A_PIN       6
+#define NROTATE_PIN        7
+#define SHIFT_PIN          8
+#define SPACE_PIN          9
+#define TAB_PIN            10
+#define CARRIAGE_RET_PIN   11
+#define CHAR_SEND_PIN      12
+#define BELL_PIN           13
+
+#define DEFUALT_CHAR_COUNT 44   // Number of characters per char_map
+#define TIME_TO_PRINT_CHAR 65   // Milliseconds
+#define BREATHING_TIME     5    // Time between the characters
 #define BAUD_RATE          1200 // Slowest baudrate Arduino serial monitor
                                 // will work with
 #define QUEUE_LENGTH       512  // 512 characters can be buffered, try to get
                                 // a lower baudrate
+#define ESCAPE             27   // ASCII Character used to pause communication (27 = Escape key)
+
+#define SOLENOID_PULL      LOW
+#define SOLENOID_RELEASE   HIGH
 
 cppQueue character_queue(sizeof(char), QUEUE_LENGTH, FIFO, true);
 
 int character_sent = 0;
+int escaped = 0;
 unsigned long last_time = 0;
 
 // Designed for Standard U.S. 7XX type elements
@@ -62,15 +70,18 @@ char char_map_upper[]  = { // Home
                                           };
 
 void setup() {
-  pinMode(TILT1_PIN,        OUTPUT);
-  pinMode(TILT2_PIN,        OUTPUT);
-  pinMode(ROTATE1_PIN,      OUTPUT);
-  pinMode(ROTATE2_PIN,      OUTPUT);
-  pinMode(ROTATE2A_PIN,     OUTPUT);
-  pinMode(NROTATE_PIN,      OUTPUT);
-  pinMode(SHIFT_PIN,        OUTPUT);
-  pinMode(SPACE_PIN,        OUTPUT);
-  pinMode(CHAR_SEND_PIN,    OUTPUT);
+  pinMode(TILT1_PIN,               OUTPUT);
+  pinMode(TILT2_PIN,               OUTPUT);
+  pinMode(ROTATE1_PIN,             OUTPUT);
+  pinMode(ROTATE2_PIN,             OUTPUT);
+  pinMode(ROTATE2A_PIN,            OUTPUT);
+  pinMode(NROTATE_PIN,             OUTPUT);
+  pinMode(SHIFT_PIN,               OUTPUT);
+  pinMode(SPACE_PIN,               OUTPUT);
+  pinMode(TAB_PIN,                 OUTPUT);
+  pinMode(CARRIAGE_RET_PIN,        OUTPUT);
+  pinMode(CHAR_SEND_PIN,           OUTPUT);
+  pinMode(BELL_PIN,                OUTPUT);
   
   Serial.begin(BAUD_RATE);
   while (!Serial) { }
@@ -79,21 +90,39 @@ void setup() {
 }
 
 void disable_all_pins() {
-  digitalWrite(TILT1_PIN,        LOW);
-  digitalWrite(TILT2_PIN,        LOW);
-  digitalWrite(ROTATE1_PIN,      LOW);
-  digitalWrite(ROTATE2_PIN,      LOW);
-  digitalWrite(ROTATE2A_PIN,     LOW);
-  digitalWrite(NROTATE_PIN,      LOW);
-  digitalWrite(SHIFT_PIN,        LOW);
-  digitalWrite(SPACE_PIN,        LOW);
-  digitalWrite(CHAR_SEND_PIN,    LOW);
+  digitalWrite(TILT1_PIN,               SOLENOID_RELEASE);
+  digitalWrite(TILT2_PIN,               SOLENOID_RELEASE);
+  digitalWrite(ROTATE1_PIN,             SOLENOID_RELEASE);
+  digitalWrite(ROTATE2_PIN,             SOLENOID_RELEASE);
+  digitalWrite(ROTATE2A_PIN,            SOLENOID_RELEASE);
+  digitalWrite(NROTATE_PIN,             SOLENOID_RELEASE);
+  digitalWrite(SHIFT_PIN,               SOLENOID_RELEASE);
+  digitalWrite(SPACE_PIN,               SOLENOID_RELEASE);
+  digitalWrite(TAB_PIN,                 SOLENOID_RELEASE);
+  digitalWrite(CARRIAGE_RET_PIN,        SOLENOID_RELEASE);
+  digitalWrite(CHAR_SEND_PIN,           SOLENOID_RELEASE);
+  digitalWrite(BELL_PIN,                SOLENOID_PULL);
 }
 
 void special_character(char c) {
   switch (c) {
   case ' ': {
-    digitalWrite(SPACE_PIN, HIGH);
+    digitalWrite(SPACE_PIN, SOLENOID_PULL);
+    character_sent = 1;
+    last_time = millis();
+    break;
+  }
+  
+  case '\t': {
+    digitalWrite(TAB_PIN, SOLENOID_PULL);
+    character_sent = 1;
+    last_time = millis();
+    break;
+  }
+  
+  case '\n':
+  case '\r': {
+    digitalWrite(CARRIAGE_RET_PIN, SOLENOID_PULL);
     character_sent = 1;
     last_time = millis();
     break;
@@ -108,7 +137,10 @@ void send_character() {
     char c = 0;
     if (!character_queue.pop(&c))
       return;
-  
+
+    // Echo character back to dumb terminal
+    Serial.print(c);
+    
     int shift = 0;
     int position = -1;
     for (int i = 0; i < DEFUALT_CHAR_COUNT; i++) {
@@ -135,28 +167,28 @@ void send_character() {
       return;
     }
 
-    digitalWrite(SHIFT_PIN, shift);
+    digitalWrite(SHIFT_PIN, shift ? SOLENOID_PULL : SOLENOID_RELEASE);
 
     int tilt = position % 4;
     int rotation = 0;
 
     if ((position / 4) >= 1 && (position / 4) <= 5) {
       rotation = (position / 4); // The character is found in the negative field
-      digitalWrite(NROTATE_PIN, HIGH); // Tell hardware this is a negative rotation
+      digitalWrite(NROTATE_PIN, SOLENOID_PULL); // Tell hardware this is a negative rotation
     } else if ((position / 4) >= 6 && (position / 4) <= 10) {
       rotation = ((position / 4) - 5); // The character is found in the positive field
     }
     
     // Encode tilt and rotation variables into the pins
-    digitalWrite(TILT1_PIN, (tilt & 1));
-    digitalWrite(TILT2_PIN, ((tilt >> 1) & 1));
+    digitalWrite(TILT1_PIN, (tilt & 1) ? SOLENOID_PULL : SOLENOID_RELEASE);
+    digitalWrite(TILT2_PIN, ((tilt >> 1) & 1) ? SOLENOID_PULL : SOLENOID_RELEASE);
 
-    digitalWrite(ROTATE1_PIN, (rotation & 1));
-    digitalWrite(ROTATE2_PIN, ((rotation >> 1) & 1));
-    digitalWrite(ROTATE2A_PIN, ((rotation >> 2) & 1));
+    digitalWrite(ROTATE1_PIN, (rotation & 1) ? SOLENOID_PULL : SOLENOID_RELEASE);
+    digitalWrite(ROTATE2_PIN, ((rotation >> 1) & 1) ? SOLENOID_PULL : SOLENOID_RELEASE);
+    digitalWrite(ROTATE2A_PIN, ((rotation >> 2) & 1) ? SOLENOID_PULL : SOLENOID_RELEASE);
 
     // Pull character send solenoid
-    digitalWrite(CHAR_SEND_PIN, HIGH);
+    digitalWrite(CHAR_SEND_PIN, SOLENOID_PULL);
     
     // Set "character_sent" to 1
     character_sent = 1;
@@ -164,20 +196,33 @@ void send_character() {
     last_time = millis();
   }
 
-  // Add if statement here to see if enough time has elapsed, and in which case
-  // "character_sent" will be set to 0
-  // "enough" = typewriter has most likely printed the character
-  if (millis() - last_time >= TIME_TO_PRINT_CHAR && character_sent == 1) {
-    character_sent = 0;
+  // See if character print cycle has elapsed
+  if (millis() - last_time >= TIME_TO_PRINT_CHAR && character_sent == 1)
     disable_all_pins();
-  }
+
+  // Breath
+  if (millis() - last_time >= (TIME_TO_PRINT_CHAR + BREATHING_TIME) && character_sent == 1)
+    character_sent = 0;
+    
   // Return back to main loop to buffer in more characters
 }
 
 void loop() {
   // Place characters into a buffer
   char c = -1;
-  if (Serial.available() && (c = (char)Serial.read()) != -1)
+
+  if (Serial.available() > 0)
+    c = (char)Serial.read();
+
+  if (c == ESCAPE)
+    escaped = !escaped;
+
+  if (escaped) {
+    disable_all_pins();
+    return;
+  }
+  
+  if (c != -1)
     character_queue.push(&c);
   
   // Send the next character in the queue
